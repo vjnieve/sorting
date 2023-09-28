5cd94dadd7cb291b84c843fa96497c34372f2830
vjnieve <vjnieve.vn@gmail.com>
2023-09-28 10:35:27 -0500

    feat: code challenge

---
+++ src/pages/api/fetchData.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const parseData = (result = [], row = []) => {
+  const [created_at, filename] = row;
+
+  return result.concat({ created_at, filename });
+};
+export default function handler(_req, res) {
+  // Perform server action
+
+  let result = [];
+
+  // read the file
+  fs.createReadStream("./data.csv")
+    .pipe(parse({ delimiter: ";", from_line: 2 }))
+    .on("data", function (row) {
+      // parse the fiel
+      result = parseData(result, row);
+    })
+    .on("end", function () {
+      res.send(result);
+    });
+}
