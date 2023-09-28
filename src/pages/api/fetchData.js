45170447ec5bfea58e752a3a2e8cd7b46b0b848a
vjnieve <vjnieve.vn@gmail.com>
2023-09-28 11:06:09 -0500

    fix: file path

--- src/pages/api/fetchData.js
+++ src/pages/api/fetchData.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const { parse } = require("csv-parse");
 
 const parseData = (result = [], row = []) => {
@@ -9,20 +10,22 @@ const parseData = (result = [], row = []) => {
 export default function handler(_req, res) {
   // Perform server action
 
-  let result = [];
+  try {
+    let result = [];
 
-  console.log("joining here");
+    const csvDirectory = path.join(process.cwd(), "");
 
-  return res.sendStatus(200);
-
-  // read the file
-  fs.createReadStream("./data.csv")
-    .pipe(parse({ delimiter: ";", from_line: 2 }))
-    .on("data", function (row) {
-      // parse the fiel
-      result = parseData(result, row);
-    })
-    .on("end", function () {
-      res.send(result);
-    });
+    // read the file
+    fs.createReadStream(csvDirectory + "/data.csv")
+      .pipe(parse({ delimiter: ";", from_line: 2 }))
+      .on("data", function (row) {
+        // parse the fiel
+        result = parseData(result, row);
+      })
+      .on("end", function () {
+        res.send(result);
+      });
+  } catch (err) {
+    console.log(err);
+  }
 }
